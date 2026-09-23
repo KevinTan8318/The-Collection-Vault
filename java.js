def22dc1134be0321b5bd24d277b18c141cd5c93
KevinTan8318 <k.tan14@share.epsb.ca>
2026-09-23 09:42:27 -0600

    Update java.js

--- java.js
+++ java.js
@@ -1,3 +1,5 @@
+const liElements = document.getElementsByTagName("li");
+
 const vault = [
   {
     name: "Song1",
@@ -5,5 +7,51 @@ const vault = [
     rarity: "Common",
     rating: 50,
   },
+  {
+    name: "Song2",
+    category: "Pop",
+    rarity: "Common",
+    rating: 31,
+  },
+  {
+    name: "Song3",
+    category: "Pop",
+    rarity: "Common",
+    rating: 35,
+  },
+  {
+    name: "Song4",
+    category: "Pop",
+    rarity: "Common",
+    rating: 53,
+  },
+  {
+    name: "Song5",
+    category: "Pop",
+    rarity: "Legendary",
+    rating: 64,
+  },
+  {
+    name: "Song6",
+    category: "Pop",
+    rarity: "Uncommon",
+    rating: 35,
+  },
+  {
+    name: "Song7",
+    category: "Pop",
+    rarity: "Rare",
+    rating: 43,
+  },
+  {
+    name: "Song8",
+    category: "Pop",
+    rarity: "Epic",
+    rating: 64,
+  },
 ];
 
+for (let i = 0; i < liElements.length; i++) {
+  liElements[i].textContent = vault[i].name;
+
+}
